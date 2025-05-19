90817a57909fb14d75f88c18d630dd317045173b
eyobbaye <eyob.bay.com>
2025-05-19 14:34:46 +0300

    Section: number count & choice-us added

---
+++ script.js
@@ -0,0 +1,19 @@
+let allvalues = document.querySelectorAll(".value");
+//start forwach loop for displaying the animated values
+allvalues.forEach((singlevalue) => {
+  let startValue = 0;
+  let endValue = parseInt(singlevalue.getAttribute("data-value"));
+  let duration = Math.floor(2000 / endValue);
+
+  // counter for inceasing the values and display
+
+  let counter = setInterval(function () {
+    startValue += 1;
+    singlevalue.textContent = startValue;
+    //Clearing the interval
+
+    if (startValue == endValue) {
+      clearInterval(counter);
+    }
+  }, duration);
+});
