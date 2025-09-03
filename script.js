cf1bad4a56cb4741687a667467ee77e0dbaf278d
GeethikaMaddi <mgeethika490@gmail.com>
2025-09-03 14:40:10 +0530

    Fix contact form layout and add JS functionality

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
+const form = document.getElementById("contactForm");
+const scriptURL = "https://script.google.com/macros/s/AKfycbxs0VW3hE_w72hpc8pMwkAkQcTvnPHcYL7nYOoyytpyQMnGHPbSbODCXTO3Q_lX2d-0/exec"; // replace with your Apps Script URL
+const msg = document.getElementById("msg");
+
+form.addEventListener("submit", e => {
+  e.preventDefault(); // prevent page reload
+  fetch(scriptURL, { method: "POST", body: new FormData(form) })
+    .then(response => {
+      msg.innerHTML = "Message sent successfully!";
+      msg.style.color = "green";
+      form.reset();
+    })
+    .catch(error => {
+      msg.innerHTML = "Error sending message!";
+      msg.style.color = "red";
+      console.error("Error!", error.message);
+    });
+});
+
 fetch("/component/navbar/nav.html")
   .then((response) => response.text())
   .then((data) => {
